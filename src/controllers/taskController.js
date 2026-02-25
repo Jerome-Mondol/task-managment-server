9e9576c58ce61b5ccf8eaeba7c966c5961b15be5
Jerome Mondol <jeromemondol11@gmail.com>
2026-02-25 22:54:23 +0600

    task create and update doone

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -42,7 +42,7 @@ export const updateTask = async (req, res) => {
         const task = await Task.findOneAndUpdate(
             { _id: id, user: req.user.id },
             { title, description, status },
-            { new: true, runValidators: true }
+            { returnDocument: 'after', runValidators: true }
         );
 
         if(!task) {
@@ -52,6 +52,22 @@ export const updateTask = async (req, res) => {
         res.status(200).json({ message: "Task updated successfully", task });
     }
 
+    catch(error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
+
+export const deleteTask = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const task = await Task.findOneAndDelete({ _id: id, user: req.user.id });
+
+        if(!task) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+
+        return res.status(200).json({ message: "Task deleted successfully" });
+    }
     catch(error) {
         res.status(500).json({ message: "Server error", error: error.message });
     }
